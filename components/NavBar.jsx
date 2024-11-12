87b45600f6bb89d9d07a6ed7ebb5c504be5b034e
harry <hillarybond10@gmail.com>
2024-11-12 19:59:46 +0100

    Navbar change

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -240,15 +240,26 @@ const NavBar = () => {
                           </Link>
 
                         )}
-                    {!session && (
-                      <button
-                      className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
-                  >
-                      
+                  {!session && (
+            <div className='hidden md:block md:ml-6'>
+              <div className='flex items-center'>
+                {providers &&
+                  Object.values(providers).map((provider) => (
+                    <button
+                      key={provider.name}
+                      onClick={() => 
+                         signIn(provider.id)                                        
+                        }
+                      className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-3'
+                    >
+                      <FaGoogle className='text-white mr-2' />
                       <span>Login or Register</span>
-                  </button>
-
-                    )}
+                    </button>
+                  ))}
+                  
+              </div>
+            </div>
+          )}
                     
                     </div>
                 </div>
